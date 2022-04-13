95ba0d3f481ec72eccfdd1798f32f7a59c8919de
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-12 21:52:05 -0300

    feat: purchases - use implementations in http module

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -3,14 +3,33 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
+import { DatabaseModule } from 'src/database/database.module';
+import { CustomersService } from 'src/services/customers.service';
+import { ProductsService } from 'src/services/products.service';
+import { PurchasesService } from 'src/services/purchases.service';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    DatabaseModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
-      autoSchemaFile: path.resolve(process.cwd(), '/src/schema.gql'),
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+  ],
+  providers: [
+    //Resolvers
+    ProductsResolver,
+    PurchasesResolver,
+    CustomersResolver,
+
+    //Services
+    ProductsService,
+    PurchasesService,
+    CustomersService,
   ],
 })
 export class HttpModule {}
