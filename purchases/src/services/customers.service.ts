000015823cbf39cc5d82757b931b2ff38b2e9046
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-12 21:47:21 -0300

    feat: purchases - implements services

---
+++ purchases/src/services/customers.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma/prisma.service';
+
+interface ICreateCustomerParams {
+  authUserId: string;
+}
+
+@Injectable()
+export class CustomersService {
+  constructor(private prisma: PrismaService) {}
+
+  getCustomerByAuthUserId(id: string) {
+    return this.prisma.customer.findUnique({
+      where: {
+        authUserId: id,
+      },
+    });
+  }
+
+  async createCustomer({ authUserId }: ICreateCustomerParams) {
+    return await this.prisma.customer.create({
+      data: {
+        authUserId,
+      },
+    });
+  }
+}
