f67ca1a1e04b250476db1f933c29061d62b8e844
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-24 16:23:23 -0300

    feat: classroom - use services and providers

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -3,14 +3,33 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
+import { DatabaseModule } from 'src/database/database.module';
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/students.service';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    DatabaseModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
-      autoSchemaFile: path.resolve(process.cwd(), '/src/schema.gql'),
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+  ],
+  providers: [
+    //Resolvers
+    CoursesResolver,
+    EnrollmentsResolver,
+    StudentsResolver,
+
+    //Services
+    CoursesService,
+    EnrollmentsService,
+    StudentsService,
   ],
 })
 export class HttpModule {}
