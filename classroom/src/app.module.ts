570dea37641a36733ed78c2e349f96fc9b61d568
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-30 08:55:12 -0300

    feat: classroom - add Messaging module

--- classroom/src/app.module.ts
+++ classroom/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
 import { PrismaService } from './database/prisma/prisma.service';
+import { MessagingModule } from './messaging/messaging.module';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [DatabaseModule, HttpModule, MessagingModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
