6bd5b34280f26c206a89e43fed6b0ec101e028a4
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-30 10:52:33 -0300

    feat: web - initial use example queries GraphQL

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,14 +1,31 @@
-import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useGetProductsQuery, useMeQuery } from "../../graphql/generated/graphql";
+import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page";
+import { withApollo } from "../../lib/withApollo";
 
-export default function Home() {
+function Home({ data }) {
     const { user } = useUser();
 
+    const { data: me } = useMeQuery();
+
     return(
-        <div>
+        <div className="text-violet-500">
             <h1>Hello World</h1>
+            <pre>{ JSON.stringify(data, null, 2) }</pre>
             <pre>{ JSON.stringify(user, null, 2) }</pre>
+            <pre>{ JSON.stringify(me, null, 2) }</pre>
         </div>
     )
 }
 
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+    getServerSideProps: async (ctx) => {
+      return {
+        props: {}
+      }
+    }
+  });
+
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
