8653668b480975055278167d5ad744e31fb38b7d
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-04-30 08:58:04 -0300

    feat: purchases - send message to Kafka

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { KafkaService } from 'src/messaging/kafka.service';
 import { PrismaService } from '../database/prisma/prisma.service';
 
 interface ICreatePurchaseParams {
@@ -8,7 +9,7 @@ interface ICreatePurchaseParams {
 
 @Injectable()
 export class PurchasesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private kafka: KafkaService) {}
 
   listAllPurchases() {
     return this.prisma.purchase.findMany({
@@ -40,11 +41,30 @@ export class PurchasesService {
       throw new Error('Product not found.');
     }
 
-    return await this.prisma.purchase.create({
+    const purchase = await this.prisma.purchase.create({
       data: {
         customerId,
         productId,
       },
     });
+
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        id: customerId,
+      },
+    });
+
+    this.kafka.emit('purchases.new-purchase', {
+      customer: {
+        authUserId: customer.authUserId,
+      },
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug,
+      },
+    });
+
+    return purchase;
   }
 }
